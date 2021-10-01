adf5f9e79157c31d11e0b7a83729e5edb69d5857
David Ciarletta <david.ciarletta@ventrom.com>
2021-10-01 09:46:08 -0400

    use event.offsetX and offsetY if available to support events in views that are scaled higher in the DOM

--- src/util/point.js
+++ src/util/point.js
@@ -1,4 +1,10 @@
 export default function(event, el) {
+  if (isFinite(event.offsetX) && !isNaN(event.offsetX)) {
+    return [
+      event.offsetX - (el.clientLeft || 0),
+      event.offsetY - (el.clientTop || 0)
+    ];
+  }
   var rect = el.getBoundingClientRect();
   return [
     event.clientX - rect.left - (el.clientLeft || 0),
